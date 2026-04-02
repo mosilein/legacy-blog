b46263a07887a6f19ddafdaae10dc1f694bd6ec7
mosilein <mosilein92@gmail.com>
2026-04-01 23:02:22 -0300

    Fix Quartz baseUrl for sitemap

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "blog.legacychronicles.life",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
